df9ccacbe570d04d2aa063a51d63cdbe256db29d
sjodie1157 <sjodie1157@gmail.com>
2024-02-17 23:59:46 +0200

    folder strcuter and store test

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,23 @@
 import { createStore } from 'vuex'
+const serverUrl = 'http://localhost:4500/Users'
 
 export default createStore({
   state: {
+    Users: null,
   },
   getters: {
   },
   mutations: {
+    setUsers(state, value) {
+      state.Users = value
+    }
   },
   actions: {
+    async fetchUsers(context) {
+      let res = await fetch(serverUrl)
+      let users = await res.json()
+      context.commit('setUsers', users)
+    }
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-if="!Users.length">
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    <div v-else>
+      <ul>
+        <li v-for=" user  in  Users " :key="user.userID">{{ user.firstName }} {{ user.lastName }}</li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  computed: {
+    Users() {
+      return this.$store.state.Users || [];
+    }
+  },
+  mounted() {
+    try {
+      setTimeout(() => {
+        this.$store.dispatch('fetchUsers');
+      }, 3000);
+      this.loading = false;
+    } catch (error) {
+      console.error('Error fetching User data', error);
+      this.loading = false;
+    }
+  }
+};
+</script>
