99a4a1c419c7de0b88b7d8f9fbfdde2e0dee8336
sjodie1157 <sjodie1157@gmail.com>
2024-02-19 14:22:31 +0200

    fixed user config pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,54 +1,15 @@
 <template>
-  <div class="about">
-    <div v-if="!Users.length">
-      <div class="spinner-border text-primary" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-    </div>
-    <div v-else>
-      <ul>
-        <li v-for="user in Users" :key="user.userID">
-          {{ user.firstName }} {{ user.lastName }}
-          <button @click="deleteUser(user.userID)">Delete</button>
-        </li>
-      </ul>
-    </div>
+  <div>
+
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-  computed: {
-    Users() {
-      return this.$store.state.Users || [];
-    }
-  },
-  methods: {
-    async deleteUser(userID) {
-      try {
-        await fetch(`http://localhost:4500/Users/removeUser/${userID}`, {
-          method: 'DELETE'
-        });
-        this.$store.dispatch('fetchUsers');
-        alert('User has been removed')
-      } catch (error) {
-        console.error('Error deleting user', error);
-      }
-    }
-  },
-  mounted() {
-    try {
-      setTimeout(() => {
-        this.$store.dispatch('fetchUsers');
-      }, 3000);
-      this.loading = false;
-    } catch (error) {
-      console.error('Error fetching User data', error);
-      this.loading = false;
-    }
+  export default {
+    
   }
-};
 </script>
+
+<style scoped>
+
+</style>
